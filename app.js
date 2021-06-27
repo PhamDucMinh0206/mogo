ac3edf667762b690df532bdc3722b4999368fce1
PhamDucMinh0206 <phanminh01jj@gmail.com>
2021-06-27 19:08:29 +0700

    toi la minh

--- app.js
+++ app.js
@@ -145,6 +145,6 @@ app.get('/',(req,res)=>{
     res.render('index',{loginName:userName})
 })
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 7000;
 app.listen(PORT);
 console.log('Server is running at: '+ PORT);
